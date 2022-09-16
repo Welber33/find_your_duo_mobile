63eea5388b1bf5ad0b823c28ad772faae15f1dd8
Welber33 <jwwork21@gmail.com>
2022-09-16 17:52:26 -0300

    NLW eSports - Stage 5 Final Round

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -15,16 +15,26 @@ import { THEME } from '../../theme';
 
 import { Heading } from '../../components/Heading';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { DuoMatch } from '../../components/DuoMatch'
 
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('')
   const navigation = useNavigation()
   const route = useRoute();
   const game = route.params as GameParams;
 
   function handleGoBack(){
     navigation.goBack()
+  }
+
+  async function getDiscordUser(adsId: string){
+    fetch(`http://192.168.18.2:3334/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => { 
+        setDiscordDuoSelected(data.discord)
+      })
   }
 
   useEffect(() => {
@@ -71,7 +81,7 @@ export function Game() {
           renderItem={({item}) => (
             <DuoCard  
               data={item}
-              onConnect={() => { }}
+              onConnect={() => { getDiscordUser(item.id) }}
             />
           )}
           horizontal
@@ -83,6 +93,12 @@ export function Game() {
               Não há anúncios publicados aqui.
             </Text>
           )}
+        />
+
+        <DuoMatch 
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
 
       </SafeAreaView>
